1c07641de2302da40e262e82c5968dc48fe6c2f5
mpsss <mephistommm@gmail.com>
2017-04-06 20:29:26 +0800

    delete docs

--- docs/api_project.js
+++
@@ -1,1 +0,0 @@
-define({  "name": "wonderful repository api",  "version": "0.1.0",  "description": "api for app and web end.",  "title": "Api",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2017-04-06T03:15:53.066Z",    "url": "http://apidocjs.com",    "version": "0.17.5"  }});
